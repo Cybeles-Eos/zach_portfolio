582757a73e6e89822d74de2640cd710df10e9fba
Dionysus <158821418+Cybeles-Eos@users.noreply.github.com>
2025-05-28 21:23:50 +0800

    add my profile

--- src/assets/pages/home.jsx
+++ src/assets/pages/home.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 
 import spiral_img from '/done.png'
 import lineSp from '/rolls.png'
-import me from '/me.jpg'
+import me from '/zach1.jpg'
 
 function Home(){
    const right_i = <svg xmlns="http://www.w3.org/2000/svg" className="mt-[.6px]" width="20" height="20" fill="var(--bg-clr)" viewBox="0 0 16 16"><path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>;   
